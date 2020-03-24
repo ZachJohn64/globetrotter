955bb36cd0142b527eeabbf5cebc3b1eda3d0bd8
ZachJohn64 <zachdjohnson64@gmail.com>
2020-03-24 07:30:23 -0400

    Created Log Entry Schema, set up Mongoose and env Variables, and started routes

---
+++ server/src/models/LogEntry.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const requiredNumber = {
+    type: Number,
+    required: true
+}
+
+
+const logEntrySchema = new Schema ({
+    title: {
+        type: String,
+        required: true
+    } ,
+    description: String,
+    comments: String,
+    image: String,
+    rating: {
+        type: number,
+        min: 0,
+        max: 10,
+        default: 0
+    },
+    latitude: {
+        ...requiredNumber,
+        min: -89.9999999999,
+        max: 89.9999999999
+    },
+    longitude: {
+        ...requiredNumber,
+        min: -179.9999999999,
+        max: 179.99999999999
+    },   
+    visitDate: {
+        required: true,
+        type: Date,
+
+    }
+}, {
+    timestamps: true,
+    
+});
+
+const LogEntry = mongoose.model('LogEntry', logEntrySchema)
+
+module.exports = LogEntry;
