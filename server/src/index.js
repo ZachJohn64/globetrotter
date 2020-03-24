aed91307071c6c8e70c82a3228cd9d746aa91ffc
ZachJohn64 <zachdjohnson64@gmail.com>
2020-03-23 21:13:14 -0400

    clead up index.js in server folder and add middlewares in own file

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const middlewares = require('./middlewares')
+
 const app = express();
 app.use(morgan('common'));
 app.use(helmet());
@@ -13,23 +15,12 @@ app.use(cors({
 app.get('/', (req, res) => {
     res.json({
         message: 'Hello World!',
-    })
-})
-
-app.use((req, res, next) => {
-    const error = new Error('Not Found - ${req.originalUrl}');
-    res.status(404);
-    next(error);
-})
-
-app.use((error, req, res, next) => {
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-    res.status(statusCode);
-    res.json({
-        message: error.message,
-        stack: process.env.NODE_ENV === 'production' ? '🥞' : error.stack
     });
 });
+
+app.use(middlewares.notFound);
+
+app.use(middlewares.errorHandler);
 
 const port = process.env.PORT || 5000;
 
