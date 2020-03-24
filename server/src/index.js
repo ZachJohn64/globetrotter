ce5182bac0126b38b5ba306f46fb5d0588524ba0
ZachJohn64 <zachdjohnson64@gmail.com>
2020-03-23 20:24:07 -0400

    Hooked up app listener and set up morgan

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+app.use(morgan('common'));
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Listening at http://localhost:5000`)
+    console.log(`Listening at http://localhost:${port}`);
 });
