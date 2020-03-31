cbdd68f0508348d7792ced81e0e467a0b28cd7b8
ZachJohn64 <zachdjohnson64@gmail.com>
2020-03-31 14:45:55 -0400

    Got the Log Entry Form to send an object

--- client/src/LogEntryForm.js
+++ client/src/LogEntryForm.js
@@ -3,16 +3,22 @@ import { useForm } from 'react-hook-form';
 const LogEntryForm = () => {
     const { register, handleSubmit } = useForm();
 
+    const onSubmit = (data) => {
+        console.log(data)
+    };
+
     return (
-        <form className="entry-form">
-            <label for="title">Title</label>
-            <input name="title" required/>
-            <label for="comments">Comments</label>
-            <textarea name="comments" rows={3}></textarea>
-            <label for="description">Description</label>
-            <textarea name="description" rows={3}></textarea>
-            <label for="visitDate">Visit Date</label>
-            <input name="visitDate" type="date" required/>
+        <form onSubmit={handleSubmit(onSubmit)} className="entry-form">
+            <label htmlFor="title">Title</label>
+            <input name="title" required ref={register}/>
+            <label htmlFor="comments">Comments</label>
+            <textarea name="comments" rows={3} ref={register}></textarea>
+            <label htmlFor="description">Description</label>
+            <textarea name="description" rows={3} ref={register}></textarea>
+            <label htmlFor="image">Image</label>
+            <input name="image" ref={register}/>
+            <label htmlFor="visitDate">Visit Date</label>
+            <input name="visitDate" type="date" required ref={register}/>
             <button type="submit">Mark Entry</button>
         </form>
     )
